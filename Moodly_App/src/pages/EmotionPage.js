fc7060fdf0f649a0d693eac1183d5ca948f5c934
elisarib <elisa.ribeiro@epitech.digital>
2024-10-22 12:05:28 +0200

    Moods
    Pictures of mood were added. Pictures not in the right folder. Need to change.

--- Moodly_App/src/pages/EmotionPage.js
+++ Moodly_App/src/pages/EmotionPage.js
@@ -17,55 +17,64 @@
 
 
 import React from 'react';
-import { View, StyleSheet} from 'react-native';
+import { View, StyleSheet, Image} from 'react-native';
 import EmotionText from '../components/atoms/EmotionText';
 
 
 const MoodTrackerPage = () => {
   // Choisis ici l'émotion que tu veux afficher
-  const selectedEmotion = 'heureux'; // Change cette valeur pour tester d'autres émotions
+  const selectedEmotion = 'frustré'; // Change cette valeur pour tester d'autres émotions
 
   const emotionData = {
     'heureux': {
       color: '#FFDD30',
       textColor: '#BC9D03',
       text: 'heureux',
+      image: require('./MoodHeureux.png'),
+      
 
     },
     'motivé': {
       color: '#FF94BD',
       textColor: '#FF307F', 
-      text: 'motivé'
+      text: 'motivé',
+      image: require('./MoodMotive.png'),
     },
     'neutre': {
       color: '#B0B0B0',
       textColor: '#737373', 
-      text: 'neutre'
+      text: 'neutre',
+      image: require('./MoodNeutre.png'),
     },
     'triste': {
       color: '#42A8FD',
       textColor: '#0968B8',
-      text: 'triste'
+      text: 'triste',
+      image: require('./MoodTriste.png'),
     },
     'énervé': {
       color: '#FF4545',
       textColor: '#A40000',
-      text: 'énervé'
+      text: 'énervé',
+      image: require('./MoodEnColere.png'),
     },
     'stressé': {
       color: '#F68A37',
       textColor: '#B04C00',
-      text: 'stressé'
+      text: 'stressé',
+      image: require('./MoodStresse.png'),
     },
     'fatigué': {
       color: '#9E76D6',
       textColor: '#692CBF',
-      text: 'fatigué'
+      text: 'fatigué',
+      image: require('./MoodFatigue.png'),
     },
     'frustré': {
       color: '#2EBB6E',
       textColor: '#156238',
-      text: 'frustré'
+      text: 'frustré',
+      image: require('./MoodFrustre.png'),
     },
   };
 
@@ -75,6 +84,7 @@ const MoodTrackerPage = () => {
   return (
     <View style={[styles.container, { backgroundColor: currentEmotion.color }]}>
       <EmotionText emotion={currentEmotion.text} color={currentEmotion.textColor} />
+      <Image source={currentEmotion.image} style={styles.image} />
     </View>
   );
 };
@@ -84,6 +94,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  image: {
+    width: 300,  // Set appropriate width for the image
+    height: 300, // Set appropriate height for the image
+    marginBottom: 20, // Add some space between the image and the text
+    resizeMode: 'contain', // Ensure the image fits inside the box
   }
 });
 
