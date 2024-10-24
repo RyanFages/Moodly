6b8377a605e4fd61e7c3380a73727b3cd637f8a3
elisarib <elisa.ribeiro@epitech.digital>
2024-10-24 08:21:51 +0200

    Wheel and Emotionpage Text
    moved text appearance and position and changed bottom Button

--- Moodly_App/src/components/atoms/BottomButton.js
+++ Moodly_App/src/components/atoms/BottomButton.js
@@ -1,31 +1,32 @@
-// Button.js
-import React from "react";
-import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet, Dimensions } from 'react-native';
 
-const Button = ({ title, onPress, style }) => {
-    return (
-        <TouchableOpacity onPress={onPress} style={[styles.button, style]}>
-            <Text style={styles.buttonText}>{title}</Text>
-        </TouchableOpacity>
-    );
+const { width } = Dimensions.get('window');
+
+const BottomButton = ({ title, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: '#3b2414',
-        alignSelf: 'stretch',
-        paddingVertical: 12,
-        borderRadius: 100, // Valeur par défaut pour les bords arrondis
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'absolute',
-        bottom:56,
-        width:'80%',
-    },
-    buttonText: {
-        color: '#fff',
-        fontSize: 16,
-    },
+  button: {
+    backgroundColor: '#3B2414',
+    paddingVertical: 12,
+    width: width * 0.8, // 80% of screen width
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 56,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
 
-export default Button;
+export default BottomButton;
